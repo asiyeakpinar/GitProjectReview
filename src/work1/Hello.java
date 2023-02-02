d268a4da51fd472e6caf6bc2dd767cc3681cd12a
asiyeakpinar <asiyeakpinar4@gmail.com>
2023-02-02 19:22:26 +0000

    Started hello future

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Work1 started");
         System.out.println("Created Git Repository ");
+        System.out.println("Local repository already published toGitHub");
+        System.out.println("Doing more work");
     }
 }
